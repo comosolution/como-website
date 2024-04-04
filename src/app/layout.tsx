b99d2c3580422fcae5eb7a12dcab948c181e8650
E R I C S C H M I D T <info@ericschmidt.de>
2024-04-04 14:53:18 +0200

    tweaks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./style/globals.css";
-import "./style/sm.css";
 import Nav from "./components/nav";
 import Footer from "./components/footer";
 import Contact from "./components/contact";
