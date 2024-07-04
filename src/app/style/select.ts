fd980b8624c4a2bd6c44ec668413b6d73fc88348
E R I C S C H M I D T <info@ericschmidt.de>
2024-07-04 13:48:44 +0200

    design tweaks

--- src/app/style/select.ts
+++ src/app/style/select.ts
@@ -24,17 +24,24 @@ export const customStyles: StylesConfig = {
   }),
   menu: (base) => ({
     ...base,
-    background: "rgb(0,0,0)",
+    backdropFilter: "blur(8px)",
+    background: "rgba(0, 0, 0, 0.7)",
     border: "1px solid rgba(var(--foreground-rgb), 0.1)",
+    boxShadow: "0 4px 8px -0 rgb(var(--background-rgb))",
     width: "100%",
   }),
   option: (base, { isFocused }) => ({
     ...base,
-    background: isFocused ? "rgba(var(--orange-rgb), 0.8)" : "",
+    background: "none",
+    color: isFocused ? "rgba(var(--orange-rgb), 1)" : "",
     cursor: "pointer",
+    fontSize: "20px",
+    padding: "4px 12px",
+    transition: "var(--default-transition)",
     ":active": {
       ...base[":active"],
       backgroundColor: "rgba(var(--orange-rgb), 1)",
+      color: "rgb(var(--foreground-rgb));",
     },
   }),
   multiValue: (base) => ({
