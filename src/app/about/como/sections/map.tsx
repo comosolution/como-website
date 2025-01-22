1d3f5becada32f71b7a83310416f6965a602bb7a
E R I C S C H M I D T <info@ericschmidt.de>
2025-01-22 15:28:11 +0100

    add @react-google-maps/api

--- src/app/about/como/sections/map.tsx
+++ src/app/about/como/sections/map.tsx
@@ -1,40 +1,38 @@
 "use client";
 import { mapStyle } from "@/app/style/map";
-import React, { useEffect, useRef } from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 export default function Map() {
-  const mapRef = useRef<any>(null);
-  const style = { height: "480px", width: "100%" };
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+  });
 
-  useEffect(() => {
-    const googleMapsScript = document.createElement("script");
-    googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`;
-    googleMapsScript.async = true;
-    googleMapsScript.defer = true;
-    googleMapsScript.addEventListener("load", async () => {
-      const map = new window.google.maps.Map(mapRef.current, {
-        center: { lat: 49.51227, lng: 11.28143 },
-        zoom: 11,
-        disableDefaultUI: true,
-        gestureHandling: "none",
-        keyboardShortcuts: false,
-        zoomControl: false,
-        styles: mapStyle,
-      });
-    });
-    document.body.appendChild(googleMapsScript);
-  }, []);
+  const mapOptions = {
+    disableDefaultUI: true,
+    gestureHandling: "none",
+    keyboardShortcuts: false,
+    zoomControl: false,
+    styles: mapStyle,
+  };
+
+  if (!isLoaded) return <></>;
 
   return (
     <div id="map" className="relative">
-      <div
-        ref={mapRef}
-        className="rounded-2xl ring-1 ring-white/10 shadow-2xl"
-        style={style}
-      />
+      <div className="w-full h-[480px] rounded-2xl ring-1 ring-white/10 shadow-2xl overflow-hidden">
+        <GoogleMap
+          mapContainerStyle={{
+            width: "100%",
+            height: "100%",
+          }}
+          zoom={11}
+          center={{ lat: 49.51227, lng: 11.28143 }}
+          options={mapOptions}
+        />
+      </div>
       <div
         className="absolute top-0 left-0 z-50 flex justify-center items-center"
-        style={style}
+        style={{ height: "480px", width: "100%" }}
       >
         <a
           href="https://www.google.com/maps/dir/?api=1&destination=CoMo+Solution+GmbH"
