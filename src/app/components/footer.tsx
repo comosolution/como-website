4e078b5e0f557e7f49d1f179bca6614dab005dd7
E R I C S C H M I D T <info@ericschmidt.de>
2025-04-15 12:25:03 +0200

    remove react reveal

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,8 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { Slide } from "react-awesome-reveal";
 import { aboutNav, portfolioNav } from "../config/nav";
-import { card, fourCols } from "../style/style";
+import { fourCols } from "../style/style";
 
 export default function Footer() {
   return (
@@ -15,57 +14,55 @@ export default function Footer() {
         className="absolute -bottom-8 -z-1 w-full footerCircles"
       />
       <main className="relative overflow-y-hidden">
-        <Slide direction="up" triggerOnce>
-          <footer className={`flex flex-col gap-8 mx-8 px-8 py-8 ${card}`}>
-            <Link href="/">
-              <header className="flex items-center gap-4 px-8">
-                <Image src="/logo.svg" width="96" height="96" alt="Logo" />
-              </header>
+        <footer className="flex flex-col gap-8 mx-8 p-8 rounded-2xl backdrop-blur-sm bg-white/70">
+          <Link href="/">
+            <header className="flex items-center gap-4 px-8">
+              <Image src="/logo.svg" width="96" height="96" alt="Logo" />
+            </header>
+          </Link>
+          <div className={fourCols}>
+            <div className="sm:col-span-2 px-8 py-4">
+              <p>CoMo Solution GmbH</p>
+              <p>Marktplatz 18</p>
+              <p>91207 Lauf a. d. Pegnitz</p>
+              <a href="mailto:info@como-solution.de">
+                <p>info@como-solution.de</p>
+              </a>
+              <Link href="tel:+4991231833700">
+                <p>+49 9123 18337-00</p>
+              </Link>
+              <Link href="https://www.linkedin.com/company/como-solution-gmbh">
+                <p>LinkedIn</p>
+              </Link>
+            </div>
+            {[portfolioNav, aboutNav].map((nav) => {
+              return (
+                <div key={nav.name} className="px-8 py-4">
+                  <p>{nav.name}</p>
+                  {nav.entries.map((entry) => {
+                    return (
+                      <Link key={entry.name} href={entry.ref}>
+                        <p>{entry.name}</p>
+                      </Link>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+          <div className={fourCols}>
+            <p className="px-8">© 2025</p>
+            <Link href="/legal/privacy" className="px-8">
+              <p>Datenschutz</p>
             </Link>
-            <div className={fourCols}>
-              <div className="sm:col-span-2 px-8 py-4">
-                <p>CoMo Solution GmbH</p>
-                <p>Marktplatz 18</p>
-                <p>91207 Lauf a. d. Pegnitz</p>
-                <a href="mailto:info@como-solution.de">
-                  <p>info@como-solution.de</p>
-                </a>
-                <Link href="tel:+4991231833700">
-                  <p>+49 9123 18337-00</p>
-                </Link>
-                <Link href="https://www.linkedin.com/company/como-solution-gmbh">
-                  <p>LinkedIn</p>
-                </Link>
-              </div>
-              {[portfolioNav, aboutNav].map((nav) => {
-                return (
-                  <div key={nav.name} className="px-8 py-4">
-                    <p>{nav.name}</p>
-                    {nav.entries.map((entry) => {
-                      return (
-                        <Link key={entry.name} href={entry.ref}>
-                          <p>{entry.name}</p>
-                        </Link>
-                      );
-                    })}
-                  </div>
-                );
-              })}
-            </div>
-            <div className={fourCols}>
-              <p className="px-8">© 2025</p>
-              <Link href="/legal/privacy" className="px-8">
-                <p>Datenschutz</p>
-              </Link>
-              <Link href="/legal/imprint" className="px-8">
-                <p>Impressum</p>
-              </Link>
-              <Link href="/legal/accessibility" className="px-8">
-                <p>Barrierefreiheit</p>
-              </Link>
-            </div>
-          </footer>
-        </Slide>
+            <Link href="/legal/imprint" className="px-8">
+              <p>Impressum</p>
+            </Link>
+            <Link href="/legal/accessibility" className="px-8">
+              <p>Barrierefreiheit</p>
+            </Link>
+          </div>
+        </footer>
       </main>
     </div>
   );
--- src/app/style/style.ts
+++ src/app/style/style.ts
@@ -4,4 +4,4 @@ export const header =
   "flex flex-col gap-4 items-center text-center lg:flex-row lg:gap-2 lg:text-left";
 export const link = "muted cursor-pointer hover:text-orange-600 transition-all";
 export const card =
-  "rounded-2xl backdrop-blur-sm bg-white/70 ring-1 ring-white/10 shadow-2xl shadow-black/10";
+  "rounded-2xl backdrop-blur-sm bg-white/70 ring-1 ring-black/10 shadow-2xl shadow-black/20";
