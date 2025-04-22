a887ba08efe7708947f7e7a320ab73e018d6eb2d
E R I C S C H M I D T <info@ericschmidt.de>
2025-04-22 15:00:47 +0200

    add angled bg

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -27,136 +27,142 @@ export default function Contact() {
     setData({ ...data, [e.target.name]: e.target.value });
 
   return (
-    <main id="contact" className={`relative z-30 ${twoCols} mb-32 px-8 py-16`}>
-      <div className="flex flex-col items-center gap-8 pt-8 lg:h-min lg:sticky lg:top-4">
-        <div className="flex flex-col text-center">
-          <header className="flex flex-col">
-            <p className="text-orange-500">
-              <b>Wir sind neugierig auf Ihre Anforderungen!</b>
-            </p>
-            <h2>
-              Nehmen Sie jetzt <br /> Kontakt zu uns auf.
-            </h2>
-          </header>
+    <div className="relative isolate">
+      <div className="absolute w-full h-full -z-10 bg-[var(--light)] clip-angled" />
+      <main
+        id="contact"
+        className={`relative z-30 ${twoCols} mb-32 px-8 py-32`}
+      >
+        <div className="flex flex-col items-center gap-8 pt-8 lg:h-min lg:sticky lg:top-4">
+          <div className="flex flex-col text-center">
+            <header className="flex flex-col">
+              <p className="text-orange-500">
+                <b>Wir sind neugierig auf Ihre Anforderungen!</b>
+              </p>
+              <h2>
+                Nehmen Sie jetzt <br /> Kontakt zu uns auf.
+              </h2>
+            </header>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-2 items-center">
+            <Button
+              variant="light"
+              component="a"
+              href="mailto:info@como-solution.de"
+              leftSection={<IconMail size={16} />}
+            >
+              info@como-solution.de
+            </Button>
+            <Button
+              variant="transparent"
+              component="a"
+              href="tel:+4991231833700"
+              leftSection={<IconPhone size={16} />}
+            >
+              +49 9123 18337-00
+            </Button>
+          </div>
         </div>
-        <div className="flex flex-col sm:flex-row gap-2 items-center">
-          <Button
-            variant="light"
-            component="a"
-            href="mailto:info@como-solution.de"
-            leftSection={<IconMail size={16} />}
-          >
-            info@como-solution.de
-          </Button>
-          <Button
-            variant="transparent"
-            component="a"
-            href="tel:+4991231833700"
-            leftSection={<IconPhone size={16} />}
-          >
-            +49 9123 18337-00
-          </Button>
-        </div>
-      </div>
-      {!success ? (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            fetch(`${HOOK_API}&src=kontaktformular`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(
-                {
-                  name: `${data.firstName} ${data.lastName}`,
-                  company: data.company,
-                  email: data.email,
-                  phone: data.phone,
-                  page: pathname,
-                  message: data.message,
+        {!success ? (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              fetch(`${HOOK_API}&src=kontaktformular`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
                 },
-                null,
-                2
-              ),
-            })
-              .then((res) => res.text())
-              .then(() => {
-                setSuccess(true);
+                body: JSON.stringify(
+                  {
+                    name: `${data.firstName} ${data.lastName}`,
+                    company: data.company,
+                    email: data.email,
+                    phone: data.phone,
+                    page: pathname,
+                    message: data.message,
+                  },
+                  null,
+                  2
+                ),
               })
-              .catch((error) => console.error(error));
-          }}
-          className={`flex flex-col gap-4 p-8 ${card} shadow-2xl shadow-orange-500/20`}
-        >
-          <h4>Ihre Kontaktanfrage</h4>
-          <div className={twoCols}>
-            <TextInput
-              name="firstName"
-              label="Vorname"
-              onChange={handleChange}
-              withAsterisk
-            />
-            <TextInput
-              name="lastName"
-              label="Nachname"
-              onChange={handleChange}
-              withAsterisk
-            />
-          </div>
-          <TextInput
-            name="company"
-            label="Unternehmen"
-            onChange={handleChange}
-            withAsterisk
-          />
-          <div className={twoCols}>
-            <TextInput
-              name="email"
-              label="E-Mail"
-              onChange={handleChange}
-              withAsterisk
-            />
-            <TextInput name="phone" label="Telefon" onChange={handleChange} />
-          </div>
-          <Textarea
-            label="Ihre Nachricht"
-            name="message"
-            placeholder="Wie können wir Ihnen weiterhelfen?"
-            rows={4}
-            onChange={handleChange}
-            withAsterisk
-          />
-          <Checkbox
-            label={
-              <>
-                Ich habe die{" "}
-                <Link href="/legal/privacy" target="_blank">
-                  Datenschutzhinweise
-                </Link>{" "}
-                zur Kenntnis genommen. Ich stimme zu, dass meine Angaben und
-                Daten zur Beantwortung meiner Anfrage elektronisch erhoben und
-                gespeichert werden.
-              </>
-            }
-            checked={privacy}
-            onChange={() => setPrivacy(!privacy)}
-          />
-          <Button
-            disabled={
-              data.firstName.length === 0 ||
-              data.lastName.length === 0 ||
-              data.company.length === 0 ||
-              !validateEmail(data.email) ||
-              data.message.length === 0 ||
-              !privacy
-            }
+                .then((res) => res.text())
+                .then(() => {
+                  setSuccess(true);
+                })
+                .catch((error) => console.error(error));
+            }}
+            className={`flex flex-col gap-4 p-8 ${card} shadow-2xl shadow-orange-500/20`}
           >
-            Jetzt Kontakt aufnehmen
-          </Button>
-        </form>
-      ) : (
-        <FormSuccess />
-      )}
-    </main>
+            <h4>Ihre Kontaktanfrage</h4>
+            <div className={twoCols}>
+              <TextInput
+                name="firstName"
+                label="Vorname"
+                onChange={handleChange}
+                withAsterisk
+              />
+              <TextInput
+                name="lastName"
+                label="Nachname"
+                onChange={handleChange}
+                withAsterisk
+              />
+            </div>
+            <TextInput
+              name="company"
+              label="Unternehmen"
+              onChange={handleChange}
+              withAsterisk
+            />
+            <div className={twoCols}>
+              <TextInput
+                name="email"
+                label="E-Mail"
+                onChange={handleChange}
+                withAsterisk
+              />
+              <TextInput name="phone" label="Telefon" onChange={handleChange} />
+            </div>
+            <Textarea
+              label="Ihre Nachricht"
+              name="message"
+              placeholder="Wie können wir Ihnen weiterhelfen?"
+              rows={4}
+              onChange={handleChange}
+              withAsterisk
+            />
+            <Checkbox
+              label={
+                <>
+                  Ich habe die{" "}
+                  <Link href="/legal/privacy" target="_blank">
+                    Datenschutzhinweise
+                  </Link>{" "}
+                  zur Kenntnis genommen. Ich stimme zu, dass meine Angaben und
+                  Daten zur Beantwortung meiner Anfrage elektronisch erhoben und
+                  gespeichert werden.
+                </>
+              }
+              checked={privacy}
+              onChange={() => setPrivacy(!privacy)}
+            />
+            <Button
+              disabled={
+                data.firstName.length === 0 ||
+                data.lastName.length === 0 ||
+                data.company.length === 0 ||
+                !validateEmail(data.email) ||
+                data.message.length === 0 ||
+                !privacy
+              }
+            >
+              Jetzt Kontakt aufnehmen
+            </Button>
+          </form>
+        ) : (
+          <FormSuccess />
+        )}
+      </main>
+    </div>
   );
 }
--- src/app/sections/benefits.tsx
+++ src/app/sections/benefits.tsx
@@ -6,39 +6,42 @@ import benefits from "./../data/benefits.json";
 
 export default function Benefits() {
   return (
-    <section className="flex flex-col gap-16 pt-64 pb-16">
-      <header className={`${header} justify-between`}>
-        <h5 className="lg:w-3/4 px-4">
-          Über 14 Jahre Innovation + Expertise – Ihr zuverlässiger Partner für{" "}
-          <span className={highlight}>maßgeschneiderte IT Lösungen</span> und
-          erstklassigen Support.
-        </h5>
-        <Button component="a" variant="light" href="/about/como">
-          Mehr erfahren
-        </Button>
-      </header>
-      <div className={fourCols}>
-        {benefits.map((benefit, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-start p-8 rounded-xl text-white bg-[var(--layers)]"
-            >
-              <Image
-                src={`/illustrations/circle-${index + 1}.svg`}
-                width={160}
-                height={160}
-                alt="Illustration"
-                className="-mt-24"
-              />
-              <div className="flex flex-col gap-4">
-                <h4>{benefit.title}</h4>
-                <p>{benefit.sub}</p>
+    <div className="relative isolate -mx-7">
+      <div className="absolute w-full h-full -z-10 bg-[var(--light)] clip-angled" />
+      <section className="flex flex-col gap-16 px-8 py-32">
+        <header className={`${header} justify-between`}>
+          <h5 className="lg:w-3/4 px-4">
+            Über 14 Jahre Innovation + Expertise – Ihr zuverlässiger Partner für{" "}
+            <span className={highlight}>maßgeschneiderte IT Lösungen</span> und
+            erstklassigen Support.
+          </h5>
+          <Button component="a" variant="light" href="/about/como">
+            Mehr erfahren
+          </Button>
+        </header>
+        <div className={fourCols}>
+          {benefits.map((benefit, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col items-start p-8 rounded-xl text-white bg-[var(--layers)]"
+              >
+                <Image
+                  src={`/illustrations/circle-${index + 1}.svg`}
+                  width={160}
+                  height={160}
+                  alt="Illustration"
+                  className="-mt-24"
+                />
+                <div className="flex flex-col gap-4">
+                  <h4>{benefit.title}</h4>
+                  <p>{benefit.sub}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-    </section>
+            );
+          })}
+        </div>
+      </section>
+    </div>
   );
 }
