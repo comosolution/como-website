53467ae70b3648a121be8a240884eb97059ae2ed
E R I C S C H M I D T <info@ericschmidt.de>
2025-04-24 16:32:19 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
   devIndicators: false,
   trailingSlash: true,
   images: { domains: ["images.ctfassets.net"] },
-  webpack(config) {
-    config.optimization.splitChunks.maxSize = 200000;
-    return config;
-  },
 };
 
 export default nextConfig;
