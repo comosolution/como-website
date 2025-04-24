2326397967dfab109b594fad0bac4178993a7a4b
E R I C S C H M I D T <info@ericschmidt.de>
2025-04-24 15:59:47 +0200

    remove static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   devIndicators: false,
   trailingSlash: true,
-  images: { unoptimized: true, domains: ["images.ctfassets.net"] },
+  images: { domains: ["images.ctfassets.net"] },
 };
 
 export default nextConfig;
