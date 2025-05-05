27c898a95a4725718c745bfd5f8a3d87e7577774
E R I C S C H M I D T <info@ericschmidt.de>
2025-05-05 11:05:59 +0200

    fix paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig = {
   output: "export",
   devIndicators: false,
   trailingSlash: true,
+  basePath: isGithubPages ? "/como-redesign" : "",
+  assetPrefix: isGithubPages ? "/como-redesign/" : "",
   images: { unoptimized: true, domains: ["images.ctfassets.net"] },
 };
 
