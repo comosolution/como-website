192edcf88e2959d7cd7cc2588d83c150c541045e
E R I C S C H M I D T <info@ericschmidt.de>
2025-05-06 15:31:12 +0200

    fix logo color

--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -1,17 +1,27 @@
 import { useMantineColorScheme } from "@mantine/core";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Logo() {
   const { colorScheme } = useMantineColorScheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Link href="/">
       <Image
         src={colorScheme === "dark" ? "/logo_dark.svg" : "/logo.svg"}
         alt="CoMo Logo"
-        width="96"
-        height="96"
+        width={96}
+        height={96}
       />
     </Link>
   );
