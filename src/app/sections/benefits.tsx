0f8342952a666c70171bf7b1d15e64a8141acfe9
E R I C S C H M I D T <info@ericschmidt.de>
2025-05-08 10:03:59 +0200

    rework benefits

--- src/app/sections/benefits.tsx
+++ src/app/sections/benefits.tsx
@@ -1,25 +1,33 @@
 "use client";
 import { Button } from "@mantine/core";
 import Image from "next/image";
-import { fourCols, header, highlight } from "../style/style";
+import Divider from "../components/divider";
+import { highlight, twoCols } from "../style/style";
 import benefits from "./../data/benefits.json";
 
 export default function Benefits() {
   return (
-    <div className="relative">
+    <div className="relative mt-16">
       <div className="absolute left-1/2 -translate-x-1/2 -z-10 w-screen h-full bg-[var(--light)] clip-angled" />
-      <section className="flex flex-col gap-16 px-8 py-32">
-        <header className={`${header} justify-between`}>
-          <h5 className="lg:w-3/4 px-4">
-            Über 14 Jahre Innovation + Expertise – Ihr zuverlässiger Partner für{" "}
+      <section className={`${twoCols} gap-y-32 items-center py-32 lg:py-0`}>
+        <div className="p-8">
+          <h2>Über 14 Jahre Innovation und Expertise</h2>
+          <Divider />
+          <h3>
+            Ihr zuverlässiger Partner für{" "}
             <span className={highlight}>maßgeschneiderte IT Lösungen</span> und
             erstklassigen Support.
-          </h5>
-          <Button component="a" variant="light" href="/about/como">
+          </h3>
+          <Button
+            component="a"
+            variant="light"
+            href="/about/como"
+            className="mt-8"
+          >
             Mehr erfahren
           </Button>
-        </header>
-        <div className={fourCols}>
+        </div>
+        <div className="flex flex-col gap-16">
           {benefits.map((benefit, index) => {
             return (
               <div
@@ -31,7 +39,7 @@ export default function Benefits() {
                   width={160}
                   height={160}
                   alt="Illustration"
-                  className="-mt-24"
+                  className="-mt-28"
                 />
                 <div className="flex flex-col gap-4">
                   <h4>{benefit.title}</h4>
