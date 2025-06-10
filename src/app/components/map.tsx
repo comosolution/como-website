c339cb0a242ef1e8a1d52f2e2b37e2b3cf37018f
E R I C S C H M I D T <info@ericschmidt.de>
2025-06-10 16:07:50 +0200

    fix map

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -12,7 +12,6 @@ export default function Map({
       title="Google Map"
       width="100%"
       height="480"
-      className="rounded-2xl ring-1 ring-white/10 shadow-2xl overflow-hidden"
       data-cb-src={`https://www.google.com/maps/embed/v1/place?key=${
         process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
       }&q=${encodeURIComponent(destination || "CoMo Solution GmbH")}&zoom=${
@@ -20,6 +19,12 @@ export default function Map({
       }`}
       loading="lazy"
       referrerPolicy="no-referrer-when-downgrade"
+      style={{
+        borderRadius: "1rem",
+        overflow: "hidden",
+        boxShadow:
+          "var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)",
+      }}
     />
   );
 }
