dd79895b4b267675475782cce3b649ed5bfb54e5
E R I C S C H M I D T <info@ericschmidt.de>
2025-07-03 08:39:36 +0200

    tweak

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -110,7 +110,7 @@ export default function Nav() {
             <Logo />
             <div className="flex items-center gap-2">
               <ThemeSwitch />
-              <Button variant="light" component={Link} href="/contact">
+              <Button component={Link} href="/contact">
                 Kontakt
               </Button>
             </div>
