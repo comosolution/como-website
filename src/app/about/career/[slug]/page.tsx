5d357d49d48a162cdb54762f8920e4d33388b22f
E R I C S C H M I D T <info@ericschmidt.de>
2025-08-06 16:10:37 +0200

    add cover image

--- src/app/about/career/[slug]/page.tsx
+++ src/app/about/career/[slug]/page.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import { RichTextRenderer } from "@/app/about/notes/sections/richTextRenderer";
-import { defaultPadding } from "@/app/style/style";
 import { getAllJobs, getJobBySlug, Job } from "@/app/utils/contentful";
 import { IconClock, IconMapPin } from "@tabler/icons-react";
 import { Metadata } from "next";
@@ -52,25 +51,31 @@ export default async function NotizDetailPage({
   const coverAlt = job.fields.cover?.fields?.title || job.fields.title;
 
   return (
-    <main className={`flex flex-col gap-8 items-center ${defaultPadding}`}>
-      <header className="flex flex-col items-center text-center">
-        <div className="flex flex-col md:flex-row items-center gap-4 text-orange-500 font-bold pb-2">
-          <div className="flex items-center gap-1">
-            <IconClock size={16} stroke={3} />
-            <p>{job.fields.type}</p>
+    <main className="flex flex-col gap-8 items-center">
+      <div
+        id="hero"
+        className="relative w-full min-h-[80vh] overflow-hidden -mt-32"
+      >
+        <img
+          src={coverImage || ""}
+          alt={coverAlt || ""}
+          className="absolute inset-0 w-full h-[80vh] object-cover object-center"
+        />
+        <div className="absolute inset-0 w-full h-[80vh] bg-gradient-to-tr from-[rgba(var(--background-rgb),1)] via-[rgba(var(--background-rgb),0.4)] to-transparent" />
+        <header className="relative h-[80vh] z-10 flex flex-col justify-end items-center text-center px-8 md:px-16 pb-16">
+          <div className="flex flex-col md:flex-row items-center gap-4 text-orange-500 font-bold pb-2">
+            <div className="flex items-center gap-1">
+              <IconClock size={16} stroke={3} />
+              <p>{job.fields.type}</p>
+            </div>
+            <div className="flex items-center gap-1">
+              <IconMapPin size={16} stroke={3} />
+              <p>{job.fields.location}</p>
+            </div>
           </div>
-          <div className="flex items-center gap-1">
-            <IconMapPin size={16} stroke={3} />
-            <p>{job.fields.location}</p>
-          </div>
-        </div>
-        <h1 className="text-center">{job.fields.title}</h1>
-      </header>
-      {coverImage && (
-        <div className="w-full">
-          <img src={coverImage} alt={coverAlt} className="rounded-xl mx-auto" />
-        </div>
-      )}
+          <h1 className="text-center">{job.fields.title}</h1>
+        </header>
+      </div>
       <article>
         <RichTextRenderer document={job.fields.content} />
       </article>
